247600325ff1b740db6335a83ae3866ec79c929d
bdshanto <hsib@yahoo.com>
2019-08-01 23:54:42 +0600

    adminDashboardUpdate

--- OnlineShopping/App_Start/RouteConfig.cs
+++ OnlineShopping/App_Start/RouteConfig.cs
@@ -17,49 +17,49 @@ namespace OnlineShopping
                 "Account",
                 "Account/{action}/{id}",
                 new { controller = "Account", action = "Index", id = UrlParameter.Optional },
-                new[] { "CmsShoppingCart.Controllers" }
+                new[] { "OnlineShopping.Controllers" }
             );
 
             routes.MapRoute(
                 "Cart",
                 "Cart/{action}/{id}",
                 new { controller = "Cart", action = "Index", id = UrlParameter.Optional },
-                new[] { "CmsShoppingCart.Controllers" }
+                new[] { "OnlineShopping.Controllers" }
             );
 
             routes.MapRoute(
                 "Shop",
                 "Shop/{action}/{name}",
                 new { controller = "Shop", action = "Index", name = UrlParameter.Optional },
-                new[] { "CmsShoppingCart.Controllers" }
+                new[] { "OnlineShopping.Controllers" }
             );
 
             routes.MapRoute(
                 "SidebarPartial",
                 "Pages/SidebarPartial",
                 new { controller = "Pages", action = "SidebarPartial" },
-                new[] { "CmsShoppingCart.Controllers" }
+                new[] { "OnlineShopping.Controllers" }
             );
 
             routes.MapRoute(
                 "PagesMenuPartial",
                 "Pages/PagesMenuPartial",
                 new { controller = "Pages", action = "PagesMenuPartial" },
-                new[] { "CmsShoppingCart.Controllers" }
+                new[] { "OnlineShopping.Controllers" }
             );
 
             routes.MapRoute(
                 "Default",
                 "",
                 new { controller = "Pages", action = "Index"},
-                new[] { "CmsShoppingCart.Controllers" }
+                new[] { "OnlineShopping.Controllers" }
             );
 
             routes.MapRoute(
                 "Pages",
                 "{page}",
                 new { controller = "Pages", action = "Index" },
-                new[] { "CmsShoppingCart.Controllers" }
+                new[] { "OnlineShopping.Controllers" }
             );
 
             //routes.MapRoute(
